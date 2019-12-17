9f3d22b0706ba43f1bdca9d7d782f70a663498ad
nanggo <yamsiri@gmail.com>
2019-12-18 01:36:50 +0900

    Add sorting option on query

--- tutorial-part-four/src/pages/index.js
+++ tutorial-part-four/src/pages/index.js
@@ -44,16 +44,16 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
-      totalCount
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
-          id
           frontmatter {
             title
-            date(formatString: "DD MMMM, YYYY")
+            date
           }
+          timeToRead
           excerpt
+          html
         }
       }
     }
